343395081c2e37e9ea40552e806be8509de4c5d7
ktsn <ktsn55@gmail.com>
2018-08-02 02:10:51 +0900

    allow to parse yaml format for meta data

--- packages/@birdseye/vue/src/webpack-loader.ts
+++ packages/@birdseye/vue/src/webpack-loader.ts
@@ -1,14 +1,25 @@
 import { loader } from 'webpack'
+import * as loaderUtils from 'loader-utils'
+import * as yaml from 'js-yaml'
 
 const vueBirdseyeLoader: loader.Loader = function(source, map) {
+  const options = this.resourceQuery
+    ? loaderUtils.parseQuery(this.resourceQuery)
+    : {}
+
   try {
+    let meta: string
+    if (options.lang === 'yaml' || options.lang === 'yml') {
+      meta = yaml.safeLoad(String(source))
+    } else {
+      meta = JSON.parse(String(source))
+    }
+
     this.callback(
       null,
       [
         'export default function(Component) {',
-        `  Component.options.__birdseye = ${JSON.stringify(
-          JSON.parse(String(source))
-        )}`,
+        `  Component.options.__birdseye = ${JSON.stringify(meta)}`,
         '}'
       ].join('\n'),
       map
--- packages/@birdseye/vue/test/webpack-loader.spec.ts
+++ packages/@birdseye/vue/test/webpack-loader.spec.ts
@@ -1,9 +1,14 @@
 import loader from '../src/webpack-loader'
 
-function test(content: string, cb: (err: Error, result: string) => void): void {
+function test(
+  content: string,
+  lang: string | null,
+  cb: (err: Error, result: string) => void
+): void {
   loader.call(
     {
-      callback: cb
+      callback: cb,
+      resourceQuery: lang ? '?lang=' + lang : ''
     },
     content
   )
@@ -12,11 +17,10 @@ function test(content: string, cb: (err: Error, result: string) => void): void {
 describe('webpack loader', () => {
   it('injects birdseye content', done => {
     test(
-      `
-    {
-      "name": "Test"
-    }
-    `,
+      `{
+        "name": "Test"
+      }`,
+      null,
       (_err, result) => {
         expect(result).toMatchInlineSnapshot(`
 "export default function(Component) {
@@ -29,8 +33,18 @@ describe('webpack loader', () => {
   })
 
   it('emit parse error', () => {
-    test(`{ "name": "Test", }`, err => {
+    test(`{ "name": "Test", }`, null, err => {
       expect(err.name).toBe('SyntaxError')
+    })
+  })
+
+  it('loads yaml data', () => {
+    test(`name: Test`, 'yaml', (_err, result) => {
+      expect(result).toMatchInlineSnapshot(`
+"export default function(Component) {
+  Component.options.__birdseye = {\\"name\\":\\"Test\\"}
+}"
+`)
     })
   })
 })
