fccbab6ca1cacc1baf8a1db4891fd56ea42aace6
ktsn <ktsn55@gmail.com>
2018-08-01 16:33:35 +0900

    interop es module

--- packages/@birdseye/vue/src/index.ts
+++ packages/@birdseye/vue/src/index.ts
@@ -2,6 +2,23 @@ import { Component } from 'vue'
 import { ComponentDeclaration } from '@birdseye/core'
 import { instrument as _instrument } from './instrument'
 
-export function instrument(Components: Component[]): ComponentDeclaration[] {
-  return Components.map(_instrument)
+function isNative(Ctor: any): boolean {
+  return typeof Ctor === 'function' && /native code/.test(Ctor.toString())
+}
+
+const hasSymbol =
+  typeof Symbol !== 'undefined' &&
+  isNative(Symbol) &&
+  typeof Reflect !== 'undefined' &&
+  isNative(Reflect.ownKeys)
+
+export function instrument(
+  Components: (Component | { default: Component })[]
+): ComponentDeclaration[] {
+  return Components.map((c: any) => {
+    if (c.__esModule || (hasSymbol && c[Symbol.toStringTag] === 'Module')) {
+      c = c.default
+    }
+    return _instrument(c)
+  })
 }
