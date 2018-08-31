d155f7388d290486a4480cc20dfb416be04f3edf
ktsn <ktsn55@gmail.com>
2018-08-31 17:10:45 +0900

    feat(app): add input component for string

---
+++ packages/@birdseye/app/src/components/BaseInput.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="base-input">
+    <component
+      :is="componentForType"
+      :value="value"
+      @input="$emit('input', ...arguments)"
+    />
+    <select
+      class="select-type"
+      :value="typeOfValue"
+      @change="$emit('change-type', $event.target.value)"
+    >
+      <option
+        v-for="t in availableTypes"
+        :key="t"
+        :value="t"
+      >{{ t }}</option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import InputString from './InputString.vue'
+
+const typeToComponentName: Record<string, string> = {
+  string: 'InputString'
+}
+
+const possibleTypes = Object.keys(typeToComponentName)
+
+export default Vue.extend({
+  name: 'BaseInput',
+
+  components: {
+    InputString
+  },
+
+  props: {
+    value: {
+      type: [String, Number, Boolean, Array, Object],
+      default: undefined
+    },
+
+    availableTypes: {
+      type: Array as () => string[],
+      required: true,
+      validator(types: string[]) {
+        return types.every(type => possibleTypes.indexOf(type) >= 0)
+      }
+    }
+  },
+
+  computed: {
+    typeOfValue(): string {
+      if (Array.isArray(this.value)) {
+        return 'array'
+      }
+
+      if (this.value && typeof this.value === 'object') {
+        return 'object'
+      }
+
+      return typeof this.value
+    },
+
+    componentForType(): string {
+      return typeToComponentName[this.typeOfValue]
+    }
+  }
+})
+</script>
+
+<birdseye lang="yml">
+name: BaseInput
+patterns:
+  - name: String
+    props:
+      value: string value
+      availableTypes:
+        - string
+</birdseye>
---
+++ packages/@birdseye/app/src/components/InputString.vue
@@ -0,0 +1,23 @@
+<template>
+  <input
+    type="text"
+    class="input-string"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+  >
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'InputString',
+
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
