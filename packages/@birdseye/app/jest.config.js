4c7cd1498405fbae80dd3e347cac2b6f68a9140c
ktsn <ktsn55@gmail.com>
2018-09-11 19:12:28 +0900

    test(app): configure coverage report

--- packages/@birdseye/app/jest.config.js
+++ packages/@birdseye/app/jest.config.js
@@ -14,5 +14,6 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
+  collectCoverageFrom: ['src/**/*.{ts,tsx,vue}', '!**/*.d.ts'],
   testURL: 'http://localhost/'
 }
