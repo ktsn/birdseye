a1a426c1b128d52bfef96b8bfef4815c8b7863c6
ktsn <ktsn55@gmail.com>
2019-10-17 14:33:21 +0800

    chore: upgrade vue-cli

--- packages/@birdseye/app/jest.config.js
+++ packages/@birdseye/app/jest.config.js
@@ -1,19 +1,5 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'json', 'vue', 'ts', 'tsx'],
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
-      'jest-transform-stub',
-    '^.+\\.tsx?$': 'ts-jest'
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
-  snapshotSerializers: ['jest-serializer-vue'],
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   setupFiles: ['<rootDir>/tests/unit/setup.ts'],
-  testMatch: [
-    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
-  ],
-  collectCoverageFrom: ['src/**/*.{ts,tsx,vue}', '!**/*.d.ts'],
-  testURL: 'http://localhost/'
+  collectCoverageFrom: ['src/**/*.{ts,tsx,vue}', '!**/*.d.ts']
 }
