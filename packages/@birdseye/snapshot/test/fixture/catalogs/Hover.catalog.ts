c78bdb17ec5d11a69f552fdd5786cfcdc6b41964
Katashin <ktsn55@gmail.com>
2020-09-09 23:13:11 +0800

    fix(snapshot): avoid error when specifying more than one capture (#543)

--- packages/@birdseye/snapshot/test/fixture/catalogs/Hover.catalog.ts
+++ packages/@birdseye/snapshot/test/fixture/catalogs/Hover.catalog.ts
@@ -1,17 +1,32 @@
 import { catalogFor } from '@birdseye/vue'
 import Hover from '../components/Hover.vue'
 
-export default catalogFor(Hover, 'Hover').add('hover', {
-  slots: {
-    default: 'Hover',
-  },
-  plugins: {
-    snapshot: {
-      capture: async (page, capture) => {
-        await capture()
-        await page.hover('[data-test-id=button]')
-        await capture()
+export default catalogFor(Hover, 'Hover')
+  .add('hover', {
+    slots: {
+      default: 'Hover',
+    },
+    plugins: {
+      snapshot: {
+        capture: async (page, capture) => {
+          await capture()
+          await page.hover('[data-test-id=button]')
+          await capture()
+        },
       },
     },
-  },
-})
+  })
+  .add('another hover', {
+    slots: {
+      default: 'Another Hover',
+    },
+    plugins: {
+      snapshot: {
+        capture: async (page, capture) => {
+          await capture()
+          await page.hover('[data-test-id=button]')
+          await capture()
+        },
+      },
+    },
+  })
